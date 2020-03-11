f22c83f86f6786d9ccfc8058c5250bfcb393e71f
zezzhang <joshua_zhang@foxmail.com>
2020-03-11 10:51:37 -0700

    init

--- play.sh
+++ play.sh
@@ -5,3 +5,5 @@ docker image build -t joshuaze/img2tags:1.0 .
 docker push joshuaze/img2tags:1.0
 
 docker run -m 1000m -p 8081:8081 joshuaze/img2tags:1.0
+
+docker-compose --project-name img2tags up -d
