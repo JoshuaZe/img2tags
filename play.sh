4d877aaf38e67807a71b0d3f87ccaec181ca0cf7
zezzhang <joshua_zhang@foxmail.com>
2020-03-22 17:51:38 -0700

    deploy with 1.1

--- play.sh
+++ play.sh
@@ -2,9 +2,11 @@
 
 docker image build -t joshuaze/img2tags:1.0 .
 
-docker push joshuaze/img2tags:1.0
+docker push joshuaze/img2tags:1.1
 
 docker run -m 1000m -p 8081:8081 joshuaze/img2tags:1.0
 
 docker-compose --project-name img2tags up -d
 docker-compose --project-name img2tags down
+
+docker stats
