935cac88661a506598ab2158735f8a2835b8e615
zezzhang <joshua_zhang@foxmail.com>
2020-08-24 23:52:19 -0700

    image color detection and new training code

--- image_prediction/02-train.py
+++ image_prediction/02-train.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     parser.add_argument('--hidden_size', type=int, default=512, help='dimension of LSTM hidden states')
     parser.add_argument('--num_layers', type=int, default=1, help='number of layers in LSTM')
     parser.add_argument('--num_categories', type=int, default=3, help='number of Category Classes')
-    parser.add_argument('--num_epochs', type=int, default=100)
+    parser.add_argument('--num_epochs', type=int, default=500)
     parser.add_argument('--batch_size', type=int, default=250)
     parser.add_argument('--num_workers', type=int, default=8)
     parser.add_argument('--learning_rate', type=float, default=0.001)
