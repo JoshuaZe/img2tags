bb09c4c935eedb427e37c63990a51182092ec56b
zezzhang <joshua_zhang@foxmail.com>
2020-08-25 21:09:58 -0700

    image color detection and new training code

--- play.sh
+++ play.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-ssh -i ~/Workspace/applesay applesay@139.224.19.230
-ssh -i ~/Workspace/new_ai_box root@139.224.19.230
-
 docker system prune -a -f
 
 docker image build -t joshuaze/img2tags:3.0 .
@@ -11,7 +8,12 @@ docker push joshuaze/img2tags:3.0
 
 docker run -m 1000m -p 8081:8081 joshuaze/img2tags:3.0
 
-docker-compose --project-name img2tags up -d
-docker-compose --project-name img2tags down
+ssh -i ~/Workspace/applesay applesay@139.224.19.230
+ssh -i ~/Workspace/new_ai_box root@139.224.19.230
 
+scp -i ~/Workspace/applesay -r ./models applesay@139.224.19.230:/home/applesay/image2tags
+scp -i ~/Workspace/applesay -r ./docker-compose.yml applesay@139.224.19.230:/home/applesay/image2tags
+
+docker-compose down
+docker-compose up -d
 docker stats
